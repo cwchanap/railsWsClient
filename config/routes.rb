a7be0e7111e79595248e234485e17ff98cf7cf7e
cwchanap <cwchanap@connect.ust.hk>
2019-03-08 14:47:52 +0800

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'main/index'
-  resources :articles
-  root 'main#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'main/index'
+    resources :articles do
+        resources :comments
+        get 'toggle' => 'articles#toggle'
+    end
+    root 'main#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
