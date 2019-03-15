09bd22b4fad30be1ece8b7fbc12060b8ff07881f
cwchanap <cwchanap@connect.ust.hk>
2019-03-15 18:28:51 +0800

    Add Login Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-    get 'main/index'
-    resources :articles do
-        resources :comments
-        get 'toggle' => 'articles#toggle'
-    end
-    root 'main#index'
+  post 'login/new'
+
+  resources :articles do
+      resources :comments
+      get 'toggle' => 'articles#toggle'
+  end
+  root 'login#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
