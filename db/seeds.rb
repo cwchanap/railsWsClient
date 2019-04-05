dfe5b71a0c9f80f5f1adb347fcc712552519b621
cwchanap <cwchanap@connect.ust.hk>
2019-04-05 16:24:12 +0800

    Not display user 'admin' in admin page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = User.create(username: 'admin', password: 'admin')
+admin = User.new(username: 'admin', password: 'admin').save(validate: false)
