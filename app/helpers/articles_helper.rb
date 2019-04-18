8a2046fa3891065ee5a7d1117932911b975db55a
cwchanap <cwchanap@connect.ust.hk>
2019-04-18 18:21:08 +0800

    change TZ to HKT, add heaven page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,4 +3,8 @@ module ArticlesHelper
         curr_user = session[:curr_userid]
         article_user_id == curr_user
     end
+
+    def changeTZ(time)
+        time.in_time_zone(8).strftime("%Y-%m-%d %H:%M:%S")
+    end
 end
